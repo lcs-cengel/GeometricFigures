db39ba91d043ab9baf9c3041da6377058831b892
Connor Engel <cengel@lcs.on.ca>
2024-10-28 19:11:50 -0400

    Added aditional text views to display all the computed properties

--- GeometricFigures/Views/CylinderView.swift
+++ GeometricFigures/Views/CylinderView.swift
@@ -19,17 +19,22 @@ struct CylinderView: View {
             // add an image
             
             // Label that describes what slider is for
-            Text("Radius")
-           
-            
-            // make a slider control for user imput
-            Slider(value: $currentCylinder.radius, in: 1...100, step: 1.0)
             Text("Radius: \(currentCylinder.radius.formatted())")
-            Text("Height")
+           
+            //INPUT
+            // make a slider control for user imput
+            
+            Slider(value: $currentCylinder.radius, in: 1...100, step: 1.0)
+           
+            Text("Height: \(currentCylinder.height.formatted())")
+
             Slider(value: $currentCylinder.height, in: 1...100, step: 1.0)
+            //OUTPUT
             Text("Height: \(currentCylinder.height.formatted())")
             // Label that shows current slider value
-            
+            //Label show area, volume
+            Text("Area: \(currentCylinder.totalArea.formatted())")
+            Text("Volume: \(currentCylinder.volume.formatted())")
         }
     }
 }
